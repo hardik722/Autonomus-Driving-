13bfc1511396be762a94668e269c50c65141de17
hardik722 <hardiksavaliya722@gmail.com>
2024-11-17 11:22:30 +0530

    Initial commit c++ files

---
+++ p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int num = 10101001, i, cnt = 0, sum = 0;
+    string s = to_string(num);
+    reverse(s.begin(), s.end());
+
+    for (i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '1')
+        {
+            sum += pow(2, i);
+        }
+    }
+    cout << "dec num : " << sum << endl;
+}
